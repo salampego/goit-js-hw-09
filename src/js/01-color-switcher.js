8a0a45d440b012e9d0f595a6633b97498c24b58b
Haik Ivanyan <samplelogo19@gmail.com>
2022-04-04 23:26:33 +0300

    done 3 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
 
+startBtn.addEventListener('click', generateColor);
+stopBtn.addEventListener('click', stopGenerateColor);
+
+let intervalId = null;
+
+function generateColor() {
+  startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 100);
+}
+
+function stopGenerateColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
